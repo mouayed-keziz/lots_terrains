7c9a449e990172af64ab0f0372cf758997ddddb1
Mouayed Keziz <m_keziz@estin.dz>
2025-03-04 23:12:45 +0100

    -

--- resources/views/panel/submission-answers-display.blade.php
+++ resources/views/panel/submission-answers-display.blade.php
@@ -1,9 +1,1 @@
 {{ $getChildComponentContainer()->render() }}
-
-<pre class="text-sm text-red-300">
-    {{ json_encode($this->record->answers, JSON_PRETTY_PRINT) }}
-</pre>
-
-<pre class="text-sm text-blue-300">
-    {{ json_encode($this->record->media, JSON_PRETTY_PRINT) }}
-</pre>
